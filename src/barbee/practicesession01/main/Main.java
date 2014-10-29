73dbe194838a937e6cbcf9495a8c5eee2be22162
Barbara Boros <barbee1030@gmail.com>
2014-10-29 20:11:50 +0100

    1-100 from i=0 in one line

---
+++ src/barbee/practicesession01/main/Main.java
@@ -0,0 +1,10 @@
+package barbee.practicesession01.main;
+
+/**
+ * Created by bboros on 2014.10.29..
+ */
+public class Main {
+    public static void main(String[] args){
+        for (int i=0;i<100;System.out.println(++i));
+    }
+}
