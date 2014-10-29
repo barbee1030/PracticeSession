d10c079afe5efbe9692fe482a8dfadeed0ddef2e
Barbara Boros <barbee1030@gmail.com>
2014-10-29 20:33:13 +0100

    casher v1

--- src/barbee/practicesession01/main/Main.java
+++ src/barbee/practicesession01/main/Main.java
@@ -5,12 +5,13 @@ package barbee.practicesession01.main;
  */
 public class Main {
     public static void main(String[] args){
-        int price=15940;
+        int price=15943;
         int cash=16000;
 
         if(cash==price)
             System.out.println("Have a nice day!");
         else if (cash>price)
+
             System.out.println("Here is your change: "+(cash-price));
         else
             System.out.println("It's not enough!");
