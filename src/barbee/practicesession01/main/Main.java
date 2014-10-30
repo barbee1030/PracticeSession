3b8e704ef68fe708a6d5ff466feccf195a5092e2
Barbara Boros <barbee1030@gmail.com>
2014-10-30 10:00:37 +0100

    cashier v2 - hungarian rounding

--- src/barbee/practicesession01/main/Main.java
+++ src/barbee/practicesession01/main/Main.java
@@ -5,15 +5,16 @@ package barbee.practicesession01.main;
  */
 public class Main {
     public static void main(String[] args){
-        int price=15943;
-        int cash=16000;
+        double price=15943;
+        double cash=16000;
 
-        if(cash==price)
+        if (cash==price)
             System.out.println("Have a nice day!");
-        else if (cash>price)
-
-            System.out.println("Here is your change: "+(cash-price));
-        else
-            System.out.println("It's not enough!");
+        else if (cash<price) System.out.println("It's not enough!");
+        else               //is there any better/faster/easier way to solve the „hungarian cash rounding”?
+            if (price/10 < Math.floor(price/10)+0.3) price=((int)Math.floor(price/10)*10);
+            else if (price/10 > Math.floor(price/10)+0.7) price=((int)Math.ceil(price / 10) * 10);
+            else price=((int)Math.floor(price/10)*10+5);
+        System.out.println("Here is your change: "+((int)cash-(int)price));
     }
 }
